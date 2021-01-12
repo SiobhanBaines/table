ffed59a741ca67936d3b54b6a3c7c90ff114e344
SiobhanBaines <siobhan.baines@gmail.com>
2021-01-12 21:34:24 +0000

    initial commit

---
+++ js/my_script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+//  $("h2").css("text-decoration", "underline"); 
+    $("h2").addclass("underline");
+//  $("ul").css("border", "solid 1px #ccc");
+    $("ul").addclass("border");
+
+    // odd and even?!
+    
+});
